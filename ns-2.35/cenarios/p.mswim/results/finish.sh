a605dc3b852520af359c343fbed31ab55f56a60c
Rafael Perazzo <rafaelperazzo@gmail.com>
2013-06-02 19:52:41 -0300

    Version 02/06/2013 19:52

--- ns-2.35/cenarios/p.mswim/results/finish.sh
+++
@@ -1,7 +0,0 @@
-rm -f mean.03.3.dat
-rm -f slots.03.3.dat
-arquivo="mean.03.3.dat"
-arquivo2="slots.03.3.dat"
-caminho="."
-perl ic_q.pl $caminho| sort -g > $arquivo
-perl ic_slots.pl $caminho| sort -g > $arquivo2
